1548245d37234ab24f33cec0da99896af5cfd0fa
coder-sadhin <codersadhin@gmail.com>
2024-07-29 23:00:53 +0600

    chagne some code

--- components/StairTransition.jsx
+++ components/StairTransition.jsx
@@ -16,14 +16,14 @@ const StairTransition = () => {
 
                 <motion.div 
                     initial={{opacity: 1}}
-                    animate={{
-                        opacity:0,
-                        transition:{
-                            delay:1,
-                            duration: 0.4,
-                            ease: "easeInOut"
-                        }
-                    }}
+                    // animate={{
+                    //     opacity:0,
+                    //     transition:{
+                    //         delay:1,
+                    //         duration: 0.4,
+                    //         ease: "easeInOut"
+                    //     }
+                    // }}
                     className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
                 />
             </div>
