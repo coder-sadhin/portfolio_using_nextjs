ec4248ff6379e66c0993d461a2246b6aa7c15bef
coder-sadhin <codersadhin@gmail.com>
2024-07-30 23:00:03 +0600

    add dependenci

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,4 +1,18 @@
+"use client";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+import { 
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue
+} from "@/components/ui/select";
 
 const Contact = () => {
   return (
