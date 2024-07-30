11ef5be7b6802183607d01a1ad6fbec4c537d1c6
coder-sadhin <codersadhin@gmail.com>
2024-07-30 23:05:02 +0600

    add info

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -14,6 +14,26 @@ import {
   SelectValue
 } from "@/components/ui/select";
 
+import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt} from "react-icons/fa";
+
+const info = [
+  {
+    icon: < FaPhoneAlt/>,
+    title: "Phone",
+    description:"(+86) 1867 989 4390"
+  },
+  {
+    icon: < FaEnvelope/>,
+    title: "Email",
+    description:"codersadhin@gmail.com"
+  },
+  {
+    icon: < FaMapMarkedAlt/>,
+    title: "Address",
+    description:"Jingdezhen, Jianxi, China"
+  },
+]
+
 const Contact = () => {
   return (
     <div>contact page</div>
